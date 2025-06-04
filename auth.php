9f1b2fd29274d45d7734a95251b36519ead7e496
Assil28 <assil.dkhil28@gmail.com>
2025-06-04 14:43:41 +0200

    test 7 deploy

--- auth.php
+++ auth.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-// the 6th test
+// the 7th test
 // Add CORS headers
 if (isset($_SERVER['HTTP_ORIGIN'])) {
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
