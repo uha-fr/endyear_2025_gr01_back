a2abd8ae6750c4fa01042b54477c80354c18fa54
8idwan <ridouane.announ@gmail.com>
2025-06-04 13:40:00 +0200

    Test deploiement github action

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // Configuration file for API settings
-// $apiBaseUrl = "http://localhost:8080/api";
+$apiBaseUrl = "http://localhost:8080/api";
 // $apiKey = "I8Y6179K6V47XT9G56Q434FW1EVSQP6X";
 
-$apiBaseUrl = "http://46.226.104.158";
+//$apiBaseUrl = "http://46.226.104.158";
 $apiKey = "7UHFU7I44XZLNYTZPDGG4726KB67F1RL";
